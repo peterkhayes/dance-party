7a6883bf689202563dc6aa835ec30bb1a43b3fe4
Shawn Drost <sdrost@gmail.com>
2012-12-02 22:12:42 -0800

    set up multiple dancers

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
-  var d = makeDancer(100, 400);
-  d.elem = $(d.htmlString);
-  d.elem.css("left", d.left);
-  d.elem.css("top", d.top);
-  $(".stage").append(d.elem);
+
+  var dancerSources = [makeDancer, makeDancer, makeDancer];
+  var dancers = [];
+
+  var danceFloorHeight = $("body").height();
+  var danceFloorWidth = $("body").width();
+
+  var setupDancer = function(d){
+    // make a dancer with a random position
+    var w = Math.random() * danceFloorWidth;
+    var h = Math.random() * danceFloorHeight;
+    var d = makeDancer(w, h);
+
+    // put it on the dance floor to do its thing
+    d.elem = $(d.htmlString);
+    d.elem.css("left", d.left);
+    d.elem.css("top", d.top);
+    $(".stage").append(d.elem);
+    dancers.push(d);
+  };
+
+  dancerSources.forEach(setupDancer);
+
   setInterval(function(){
-    d.step();
+    dancers.forEach(function(d){
+      d.step();
+    });
   }, 1000);
 });
