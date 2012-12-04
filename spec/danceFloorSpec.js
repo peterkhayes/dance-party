5609997cfe6f92716f41cc385445aaee0b262a62
Shawn Drost <sdrost@gmail.com>
2012-12-04 11:41:39 -0800

    danceFloor spec

--- spec/danceFloorSpec.js
+++ spec/danceFloorSpec.js
@@ -1,18 +1,42 @@
 describe("danceFloor", function() {
   var danceFloor;
+  var dancers; // object that the dance floor modifies
+  var makeFakeDancer;
+  var fakeDancer;
 
   beforeEach(function() {
-    var spy = jasmine.createSpy();
-    var kindsOfDancers = {
-      fakeDancer: spy
+    // Set up spies
+    fakeDancer = {
+      dance: jasmine.createSpy()
     };
-    var dancers = [];
+    makeFakeDancer = jasmine.createSpy().andReturn(fakeDancer);
+
+    // Set up the arguments that makeDanceFloor requires.
+    var kindsOfDancers = {
+      makeFakeDancer: makeFakeDancer
+    };
+    dancers = [];
+
     danceFloor = makeDanceFloor(kindsOfDancers, dancers);
   });
 
   it("should have width and height properties", function(){
     expect(danceFloor.width).toBeGreaterThan(0);
     expect(danceFloor.height).toBeGreaterThan(0);
+  });
+
+  describe("makeDancer", function(){
+
+    it("should make a dancer of the specified type", function(){
+      danceFloor.makeDancer("makeFakeDancer");
+      expect(makeFakeDancer).toHaveBeenCalled();
+    });
+
+    it("should call 'dance' on the created dancer", function(){
+      danceFloor.makeDancer("makeFakeDancer");
+      expect(fakeDancer.dance).toHaveBeenCalled();
+    });
+
   });
 
 });
--- src/danceFloor.js
+++ src/danceFloor.js
@@ -3,6 +3,7 @@ var makeDanceFloor = function(kindsOfDancers, dancers){
   var danceFloor = {
     width: $("body").width(),
     height: $("body").height(),
+
     makeDancer: function(dancerMakerName){
       // get the kind of dancer we're supposed to make
       var dancerMaker = kindsOfDancers[dancerMakerName];
