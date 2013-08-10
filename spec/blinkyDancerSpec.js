d276dde6eb87abb00c2a44c6b5d5fd0effafb197
Cameron Boehmer <cameron.boehmer@gmail.com>
2013-08-09 17:00:52 -0700

    pass all tests; index->dancefloor

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -20,11 +20,12 @@ describe("blinkyDancer", function() {
   });
 
   describe("dance", function(){
-    it("should call step periodically", function(){
+    it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0)
-
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
+
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
