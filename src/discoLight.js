ee26d3df977802549f1b821a6fde0216055466af
Peter Hayes <peter.k.hayes@gmail.com>
2013-10-01 12:28:54 -0700

    Disco lights move

--- src/discoLight.js
+++ src/discoLight.js
@@ -3,19 +3,25 @@ function DiscoLight(layer, phase) { // Phase is between 0 and 1, layer is a natu
   this.layer = layer;
   this.phase = phase;
 
-  // Give it a random color from the list.
-  this.$node.css("border-color", this.colors[Math.floor(Math.random()*this.colors.length)]);
+  // Adjust css from passed variables, and give a random color.
+  this.$node.css({
+    "border-color": this.colors[Math.floor(Math.random()*this.colors.length)],
+    "border-radius": 5*this.layer,
+    "border-width": 5*this.layer,
+    "opacity": 0.5 - this.layer/10
+  });
 
-  this.moveTo(this.targetPos()[0], this.targetPos()[1]);
+  this.setPosition(this.targetPos()[0], this.targetPos()[1]);
+  this.step();
 }
 
 DiscoLight.prototype.step = function() {
-
+  this.moveTo(this.targetPos()[0], this.targetPos()[1],300);
 
   setTimeout(this.step.bind(this), 300);
 };
 
-DiscoLight.prototype.colors = ["#FFCCCC", "#CCFFCC", "#CCCCFF"];
+DiscoLight.prototype.colors = ["#FFCCCC", "#CCFFCC", "#CCCCFF", "FFFFCC", "FFCCFF", "CCFFFF"];
 
 DiscoLight.prototype.setPosition = Dancer.prototype.setPosition;
 
@@ -23,7 +29,7 @@ DiscoLight.prototype.moveTo = Dancer.prototype.moveTo;
 
 DiscoLight.prototype.targetPos = function() {
   var angle = (generateAngle(10) + this.phase*Math.PI*2) % (2*Math.PI);
-  var radius = this.layer*$('body').width()/5 + 10*this.layer*this.layer;
+  var radius = this.layer*$('body').width()/10 + 10*this.layer*this.layer;
 
   var x = radius*Math.cos(angle) + $('body').width()/2;
   var y = (-1)*radius*Math.sin(angle) + $('body').height()/2;
